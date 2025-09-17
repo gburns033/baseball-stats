25547cdeadc38de9f821593d858b60b09573dd46
Gradon Burns <gradyhburns@gmail.com>
2025-09-17 15:20:03 -0500

    Switch variable storage in Hitter class

--- src/baseballStats/Hitter.java
+++ src/baseballStats/Hitter.java
@@ -1,178 +1,107 @@
 package baseballStats;
 
 import java.text.DecimalFormat;
-import java.util.HashMap;
 
 public class Hitter {
-	
-	HashMap<String, Integer> hitterStats = new HashMap<String, Integer>();
 
-	public Hitter() {
-		hitterStats.put("plateAppearances", 0);
-		hitterStats.put("atBats", 0);
-		hitterStats.put("runs", 0);
-		hitterStats.put("hits", 0);
-		hitterStats.put("singles", 0);
-		hitterStats.put("doubles", 0);
-		hitterStats.put("triples", 0);
-		hitterStats.put("homeruns", 0);
-		hitterStats.put("rbi", 0);
-		hitterStats.put("walks", 0);
-		hitterStats.put("stolenBases", 0);
-		hitterStats.put("caughtStealing", 0);
-	}
+    private int plateAppearances;
+    private int atBats;
+    private int runs;
+    private int hits;
+    private int singles;
+    private int doubles;
+    private int triples;
+    private int homeruns;
+    private int rbi;
+    private int walks;
+    private int stolenBases;
+    private int caughtStealing;
 
-	public Hitter(int plateAppearances, int atBats, int runs, int hits, int singles, int doubles, 
-				  int triples, int homeruns, int rbi, int walks, int sb, int cs) {
-		hitterStats.put("plateAppearances", plateAppearances);
-		hitterStats.put("atBats", atBats);
-		hitterStats.put("runs", runs);
-		hitterStats.put("hits", hits);
-		hitterStats.put("singles", singles);
-		hitterStats.put("doubles", doubles);
-		hitterStats.put("triples", triples);
-		hitterStats.put("homeRuns", homeruns);
-		hitterStats.put("rbi", rbi);
-		hitterStats.put("walks", walks);
-		hitterStats.put("stolenBases", sb);
-		hitterStats.put("caughtStealing", cs);
-	}
+    public Hitter() {
+        this(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    }
 
-	public void setPlateAppearances(int plateAppearances) {
-		hitterStats.put("plateAppearances", plateAppearances);
-	}
+    public Hitter(int plateAppearances, int atBats, int runs, int hits,
+                  int singles, int doubles, int triples, int homeruns,
+                  int rbi, int walks, int stolenBases, int caughtStealing) {
+        this.plateAppearances = plateAppearances;
+        this.atBats = atBats;
+        this.runs = runs;
+        this.hits = hits;
+        this.singles = singles;
+        this.doubles = doubles;
+        this.triples = triples;
+        this.homeruns = homeruns;
+        this.rbi = rbi;
+        this.walks = walks;
+        this.stolenBases = stolenBases;
+        this.caughtStealing = caughtStealing;
+    }
+    
+    public int getPlateAppearances() { return plateAppearances; }
+    public void setPlateAppearances(int plateAppearances) { this.plateAppearances = plateAppearances; }
 
-	public void setAtBats(int atBats) {
-		hitterStats.put("atBats", atBats);
-	}
-	
-	public void setruns(int runs) {
-		hitterStats.put("runs", runs);
-	}
+    public int getAtBats() { return atBats; }
+    public void setAtBats(int atBats) { this.atBats = atBats; }
 
-	public void sethits(int hits) {
-		hitterStats.put("hits", hits);
-	}
+    public int getRuns() { return runs; }
+    public void setRuns(int runs) { this.runs = runs; }
 
-	public void setsingles(int singles) {
-		hitterStats.put("singles", singles);
-	}
+    public int getHits() { return hits; }
+    public void setHits(int hits) { this.hits = hits; }
 
-	public void setdoubles(int doubles) {
-		hitterStats.put("doubles", doubles);
-	}
+    public int getSingles() { return singles; }
+    public void setSingles(int singles) { this.singles = singles; }
 
-	public void settriples(int triples) {
-		hitterStats.put("triples", triples);
-	}
+    public int getDoubles() { return doubles; }
+    public void setDoubles(int doubles) { this.doubles = doubles; }
 
-	public void setHomeruns(int homeruns) {
-		hitterStats.put("homeRuns", homeruns);
-	}
-	
-	public void setrbi(int rbi) {
-		hitterStats.put("rbi", rbi);
-	}
+    public int getTriples() { return triples; }
+    public void setTriples(int triples) { this.triples = triples; }
 
-	public void setwalks(int walks) {
-		hitterStats.put("walks", walks);
-	}
-	
-	public void setStolenBases(int sb) {
-		hitterStats.put("stolenBases", sb);
-	}
-	
-	public void setCaughtStealing(int cs) {
-		hitterStats.put("caughtStealing", cs);
-	}
-	
-	public int getPlateAppearances() {
-		return hitterStats.get("plateAppearances");
-	}
+    public int getHomeruns() { return homeruns; }
+    public void setHomeruns(int homeruns) { this.homeruns = homeruns; }
 
-	public int getAtBats() {
-		return hitterStats.get("atBats");
-	}
-	
-	public int getruns() {
-		return hitterStats.get("runs");
-	}
+    public int getRbi() { return rbi; }
+    public void setRbi(int rbi) { this.rbi = rbi; }
 
-	public int gethits() {
-		return hitterStats.get("hits");
-	}
+    public int getWalks() { return walks; }
+    public void setWalks(int walks) { this.walks = walks; }
 
-	public int getsingles() {
-		return hitterStats.get("singles");
-	}
+    public int getStolenBases() { return stolenBases; }
+    public void setStolenBases(int stolenBases) { this.stolenBases = stolenBases; }
 
-	public int getdoubles() {
-		return hitterStats.get("doubles");
-	}
+    public int getCaughtStealing() { return caughtStealing; }
+    public void setCaughtStealing(int caughtStealing) { this.caughtStealing = caughtStealing; }
 
-	public int gettriples() {
-		return hitterStats.get("triples");
-	}
+    public float calculateBattingAverage() {
+        return (atBats > 0) ? (float) hits / atBats : 0.0f;
+    }
 
-	public int getHomeruns() {
-		return hitterStats.get("homeRuns");
-	}
-	
-	public int getrbi() {
-		return hitterStats.get("rbi");
-	}
+    public float calculateOnBasePercent() {
+        return (plateAppearances > 0) ? (float) (hits + walks) / plateAppearances : 0.0f;
+    }
 
-	public int getwalks() {
-		return hitterStats.get("walks");
-	}
-	
-	public int getStolenBases() {
-		return hitterStats.get("stolenBases");
-	}
-	
-	public int getCaughtStealing() {
-		return hitterStats.get("caughtStealing");
-	}
+    public float calculateSluggingPercent() {
+        return (atBats > 0) ?
+            (float) (singles + (doubles * 2) + (triples * 3) + (homeruns * 4)) / atBats
+            : 0.0f;
+    }
 
-	public float calculateBattingAverage() {
-		int hits = hitterStats.get("hits");
-		int atBats = hitterStats.get("atBats");
-		
-		return (float)hits / atBats;
-	}
-	
-	public float calculateOnBasePercent() {
-		int hits = hitterStats.get("hits");
-		int walks = hitterStats.get("walks");
-		int plateAppearances = hitterStats.get("plateAppearances");
-		
-		return (float)(hits + walks) / plateAppearances;
-	}
-	
-	public float calculateSluggingPercent() {
-		int singles = hitterStats.get("singles");
-		int doubles = hitterStats.get("doubles");
-		int triples = hitterStats.get("triples");
-		int homeruns = hitterStats.get("homeRuns");
-		int atBats = hitterStats.get("atBats");
-		
-		return (float)(singles + (doubles * 2) + (triples * 3) + (homeruns * 4)) / atBats;
-	}
-	
-	public float calculateOPS() {
-		return calculateSluggingPercent() + calculateOnBasePercent();
-	}
-	
-	public void printSlashLine() {
-		DecimalFormat df = new DecimalFormat("#.000");
-		
-		System.out.printf("Slash line: " + df.format(calculateBattingAverage()) + "/" + df.format(calculateOnBasePercent()) + "/" + df.format(calculateSluggingPercent()));
-		System.out.println();
-	}
-	
-	public float calculateStolenBasePercent() {
-		int sb = hitterStats.get("stolenBases");
-		int cs = hitterStats.get("caughtStealing");
-		return (float)sb / (sb + cs);
-	}
+    public float calculateOPS() {
+        return calculateSluggingPercent() + calculateOnBasePercent();
+    }
+
+    public void printSlashLine() {
+        DecimalFormat df = new DecimalFormat("#.000");
+        System.out.printf("Slash line: %s/%s/%s%n",
+                df.format(calculateBattingAverage()),
+                df.format(calculateOnBasePercent()),
+                df.format(calculateSluggingPercent()));
+    }
+
+    public float calculateStolenBasePercent() {
+        int attempts = stolenBases + caughtStealing;
+        return (attempts > 0) ? (float) stolenBases / attempts : 0.0f;
+    }
 }
